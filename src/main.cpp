88a4cd48e9644ec2c3cf005f21b328c1fd20a01c
Tomáš Barták <95168802+tbartak@users.noreply.github.com>
2024-11-23 17:18:42 +0100

    feat: brightness changes based on the amount of lux

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,39 @@
 
 BH1750 lightSensor; // create instance of BH1750 class (light sensor)
 
+// TODO: potřeba nastavit uživatelem přes MQTT broker (thresholds)
+#define MAX_LUX 1000.0 // maximum light level in lux
+#define MIN_LUX 100.0 // minimum light level in lux
+
+const int LED_1 = 4; // LED connected to GPIO 4
+const int LED_2 = 2; // LED connected to GPIO 2
+const int freq = 5000; 
+const int resolution = 8;
+const int ledChannel1 = 0;
+const int ledChannel2 = 1;
+
+// TODO: linearizace úrovně svitu
+float linearization(float lux) {
+  return lux;
+}
+
+/**
+ * Calculate brightness percentage based on light level
+ */
+float brightnessCalculation(float lux) {
+  float percentage;
+  if (lux < MIN_LUX) {
+    percentage = 0;
+  } else if (lux > MAX_LUX) {
+    percentage = 100;
+  } else {
+    percentage = (lux - MIN_LUX) / (MAX_LUX - MIN_LUX) * 100;
+  }
+  return percentage;
+}
+
+// TODO: funkce an plynulý přechod mezi úrovněmi svitu
+
 /**
  * Setup function runs once when the microcontroller is powered on
  */
@@ -16,8 +49,12 @@ void setup() {
   Serial.begin(9600); // initialize serial communication at 9600 bits per second
   Wire.begin(); // initialize I2C communication
   lightSensor.begin(); // initialize light sensor
-  pinMode(4, OUTPUT); // LED connected to GPIO 4
-  pinMode(2, OUTPUT);  // LED connected to GPIO 2
+
+  // initialize LED PWM
+  ledcSetup(ledChannel1, freq, resolution);
+  ledcSetup(ledChannel2, freq, resolution);
+  ledcAttachPin(LED_1, ledChannel1);
+  ledcAttachPin(LED_2, ledChannel2);
 }
 
 void loop() {
@@ -26,16 +63,22 @@ void loop() {
     Serial.print("Light: ");
     Serial.print(lux);
     Serial.println(" lx");
+    // TODO: linearizace úrovně svitu
+    float linearizedLux = linearization(lux);
 
-    // TEST: Turn on if light level is above 100 lux
-    if (lux > 100) {
-      Serial.println("Light is on");
-      digitalWrite(4, HIGH);
-      digitalWrite(2, HIGH);
-    } else {
-      digitalWrite(4, LOW);
-      digitalWrite(2, LOW);
-    }
+    // calculate brightness percentage
+    float brightness = brightnessCalculation(linearizedLux);
+    Serial.print("Brightness: ");
+    Serial.print(brightness);
+    Serial.println("%");
 
-    delay(500); // wait half a second between measurements
+    // calculate duty cycle for PWM (0-255 int values)
+    int dutyCycle = brightness * 2.55; 
+    dutyCycle = constrain(dutyCycle, 0, 255);
+
+    // set the brightness of the LED (under the MIN_LUX the LED is off, over the MAX_LUX the LED is on full brightness)
+    ledcWrite(ledChannel1, dutyCycle);
+    ledcWrite(ledChannel2, dutyCycle);
+
+    delay(1000); // wait a second between measurements
 }
