8b177f23d8ff2f4837bb66d2fe1dfcf3fc85c5c8
Tomáš Barták <95168802+tbartak@users.noreply.github.com>
2024-11-24 16:59:20 +0100

    feat: add non-volatile storage for light thresholds and the save save and load functions needed

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,16 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <BH1750.h>
+#include <Preferences.h>
+
+#define DEFAULT_MAX_LUX 1000.0 // maximum light level in lux
+#define DEFAULT_MIN_LUX 100.0 // minimum light level in lux
+
+// TODO: these values can be changed by user through MQTT
+float MAX_LUX = DEFAULT_MAX_LUX; // maximum light level in lux
+float MIN_LUX = DEFAULT_MIN_LUX; // minimum light level in lux
 
 BH1750 lightSensor; // create instance of BH1750 class (light sensor)
-
-// TODO: potřeba nastavit uživatelem přes MQTT broker (thresholds)
-#define MAX_LUX 1000.0 // maximum light level in lux
-#define MIN_LUX 100.0 // minimum light level in lux
 
 const int LED_1 = 4; // LED connected to GPIO 4
 const int LED_2 = 2; // LED connected to GPIO 2
@@ -21,6 +25,62 @@ const int ledChannel1 = 0;
 const int ledChannel2 = 1;
 
 int previousDutyCycle = 0;
+
+Preferences preferences; // create instance of Preferences class (NVS)
+
+/**
+ * Function for saving thresholds to NVS (non-volatile storage).
+ * 
+ * @param minLux - Minimum light level threshold in lux
+ * @param maxLux - Maximum light level threshold in lux
+ */
+void saveLightThresholds(float minLux, float maxLux) {
+  if (!preferences.begin("lightThresholds", false)) // try to open NVS 'lightThresholds' namespace in read-write mode, if something goes wrong, print error message and return
+  {
+    Serial.println("Failed to open NVS for saving light thresholds");
+    return;
+  }
+  
+  preferences.begin("lightThresholds", false); 
+  preferences.putFloat("minLux", minLux); // save value
+  preferences.putFloat("maxLux", maxLux); // save value
+  preferences.end(); // close
+
+  Serial.println("Light thresholds saved");
+}
+
+/**
+ * Function for loading thresholds from NVS (non-volatile storage).
+ */
+void loadLightThresholds() {
+  if (!preferences.begin("lightThresholds", true)) // try to open NVS 'lightThresholds' namespace in read-only mode, if something goes wrong, print error message and return
+  {
+    Serial.println("Failed to open NVS for loading light thresholds. Using default values.");
+    return;
+  }
+
+  MIN_LUX = preferences.getFloat("minLux", DEFAULT_MIN_LUX); // get value or use default
+  MAX_LUX = preferences.getFloat("maxLux", DEFAULT_MAX_LUX); // get value or use default
+  preferences.end(); // close
+
+  Serial.print("Loaded light thresholds: ");
+  Serial.print(MIN_LUX);
+  Serial.print(" - ");
+  Serial.println(MAX_LUX);
+}
+
+// TODO: function to update light thresholds will be called after receiving MQTT message
+/**
+ * Function for updating light thresholds.
+ * 
+ * @param minLux - Minimum light level threshold in lux
+ * @param maxLux - Maximum light level threshold in lux
+ */
+void updateLightThresholds(float minLux, float maxLux) {
+  MIN_LUX = minLux; // internally update MIN_LUX
+  MAX_LUX = maxLux; // internally update MAX_LUX
+  saveLightThresholds(MIN_LUX, MAX_LUX); // save new thresholds to NVS for future use
+}
 
 // TODO: linearizace úrovně svitu
 float linearization(float lux) {
@@ -84,6 +144,8 @@ void setup() {
   Wire.begin(); // initialize I2C communication
   lightSensor.begin(); // initialize light sensor
 
+  loadLightThresholds(); // load light thresholds from NVS
+
   // initialize LED PWM
   ledcSetup(ledChannel1, freq, resolution);
   ledcSetup(ledChannel2, freq, resolution);
@@ -97,6 +159,7 @@ void loop() {
     Serial.print("Light: ");
     Serial.print(lux);
     Serial.println(" lx");
+    
     // TODO: linearizace úrovně svitu
     float linearizedLux = linearization(lux);
 
@@ -110,7 +173,7 @@ void loop() {
     int currentDutyCycle = brightness * 2.55; 
     currentDutyCycle = constrain(currentDutyCycle, 0, 255);
 
-    // fade between previous and current duty cycle
+    // fade between previous and current duty cycle // TODO: could think of a better option without the need to have delay in the function directly
     brightnessFade(previousDutyCycle, currentDutyCycle);
 
     // save current duty cycle for next iteration
